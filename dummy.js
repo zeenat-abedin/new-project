79c90e826395eb979e1dbc8a50809e18c459e819
zeenat <zeenu.23@gmail.com>
2024-06-21 18:33:56 +0530

    Initialize project using Create React App

---
+++ dummy.js
@@ -0,0 +1,4 @@
+// 1. Create a new project and install all the dependencies for Material UI
+// 2. Design a login page which has two input fields one email field and one password field.
+// 3. Make the design somewhat appealing
+// 4. The email and password should be validated. If the entered email is test@test.com and password 12345678 the show an alert login successful otherwise show incorrect credentials
